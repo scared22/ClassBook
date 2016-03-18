7c83934b2bc98e6593383133cd9ad36fa930ef61
kys <scared22@naver.com>
2016-03-18 22:20:24 +0900

    collection Posts 만듬 postItem 게시글폼 이미지 만듬 freepost form reform

--- Client/template/write.js
+++ Client/template/write.js
@@ -2,3 +2,17 @@ Template.write.onRendered(function(){
     $('input#input_text, textarea#textarea1').characterCounter();
     $('select').material_select();
 });
+Template.write.events({
+    'click #submit':function(e,t){
+        e.preventDefault();
+        var post ={
+            part:t.find('#Classification').value,
+            content:t.find('#textarea1').value
+        };
+        if(post.content ==0)Materialize.toast('제목 또는 내용을 입력 해주세요',1000);
+        //post._id = Posts.insert(post);
+        Materialize.toast('등록 완료!',1000);
+        if(post.part == 1) Router.go('freepost');
+        else Router.go('main');
+    }
+});
