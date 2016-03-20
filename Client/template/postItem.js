5a3490dd944bd46b16fe43137a8859be4bf6f729
kys <scared22@naver.com>
2016-03-20 22:38:58 +0900

    게시글 삭제 기능 추가 update, remove를 하기 위해서는 lib->permission 권한제어 로직만듬 이전 커밋에서 insert가 가능했던 이유는 Meteorcall 함수로 Server->post.js에서 함수로 정의 했기 때문에 allow가 필요 없었던 것이다

--- Client/template/postItem.js
+++ Client/template/postItem.js
@@ -3,3 +3,10 @@ Template.postItem.helpers({
         return this.userId === Meteor.userId();
     }
 });
+Template.postItem.events({
+    'click #postdel':function(e,t){
+        e.preventDefault();
+        var currentPostId = this._id;
+        Posts.remove(currentPostId);
+    }
+});
