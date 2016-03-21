c0c0a2f5244db8901ed1e99fe6be87dc20317507
kys <scared22@naver.com>
2016-03-21 09:38:35 +0900

    수정 할 때 한글자 등록 금지 수정 할 때 내용 말그 그이외에 다른 속성 건드릴 수 없음

--- Client/template/postEdit.js
+++ Client/template/postEdit.js
@@ -9,6 +9,10 @@ Template.postEdit.events({
         var postProperties ={
             content:t.find('#textarea1').value
         }
+        if(postProperties.content == 0){
+            Materialize.toast('빈글을 작성 할 수 없습니다.');
+            return false;
+        }
         Posts.update(currentPostId,{$set: postProperties},function(error){
             if(error){
                 alert(error.reason);
