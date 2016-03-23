8a4e3c7e5d1a5457caafa65ee4b6069577a3eaca
kys <scared22@naver.com>
2016-03-23 17:43:55 +0900

    수정을 모달로 변경 함 댓글 폼까지 완성

--- Client/template/postItem.js
+++ Client/template/postItem.js
@@ -1,3 +1,7 @@
+Template.postItem.onRendered(function(){
+    $('.modal-trigger').leanModal();
+    $('input#input_text, textarea#textarea1').characterCounter();
+});
 Template.postItem.helpers({
     myPost: function(){
         return this.userId === Meteor.userId();
@@ -8,5 +12,21 @@ Template.postItem.events({
         e.preventDefault();
         var currentPostId = this._id;
         Posts.remove(currentPostId);
+    },
+    'click #postedit':function(e,t){
+        e.preventDefault();
+        var currentPostId = this._id;
+        var postProperties ={
+            content:t.find('#textarea1').value
+        }
+        if(postProperties.content == 0){
+            Materialize.toast('빈글을 작성 할 수 없습니다.',1000);
+            return false;
+        }
+        Posts.update(currentPostId,{$set: postProperties},function(error){
+            if(error){
+                alert(error.reason);
+            } 
+        });
     }
 });
---
+++ client/template/postComment.js
@@ -0,0 +1,3 @@
+Template.postComment.onRendered(function(){
+    $('input#input_text, textarea#icon_prefix2').characterCounter();
+});
