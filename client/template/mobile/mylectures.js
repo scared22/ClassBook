a2c54de12c1ad8c7ba0961e0545b1c1d15a09262
kys <scared22@naver.com>
2016-04-21 16:42:05 +0900

    학생 수강목록 기능 완성 &in 사용하면 학생 lectures에 배열각각 인자 값을 접근 할 수 있다.

--- client/template/mobile/mylectures.js
+++ client/template/mobile/mylectures.js
@@ -2,5 +2,11 @@ Template.mylectures.helpers({
     lectures : function(){
         var user = Meteor.user();
         return Lectures.find({professor:user.profile.name});
-    } 
+    }
+});
+Template.studentlectures.helpers({
+    mylectures : function(){
+            var user = Meteor.user().profile.lectures;
+            return Lectures.find({_id: {$in: user}});
+    }
 });
